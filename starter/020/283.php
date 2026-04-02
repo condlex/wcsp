2cc6c5d93fd8f5022a5c5e60c87599eb38652806
Conditioned Reflex Builder <zinospetrel@sdf.org>
2026-04-02 17:44:17 +0700

    Create 283.php

---
+++ starter/020/283.php
@@ -0,0 +1,32 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return NULL
+     */
+    function moveZeroes(&$nums) {
+        $mover = new Zero_Mover( $nums );
+        $mover->run();
+        $mover->after_run( $nums );
+    }
+}
+
+class Zero_Mover {
+    function __construct( $numbers ) {
+        $this->numbers = $numbers;
+    }
+
+    function after_run( &$numbers ) {
+        $numbers = $this->numbers;
+    }
+
+    function run() {
+        $number_count = count( $this->numbers );
+        for ( $i = 0; $i < $number_count; $i++ ) {
+            if ( $this->numbers[$i] == 0 ) {
+                unset( $this->numbers[$i] );
+                $this->numbers[] = 0;
+            }
+        }       
+    }
+}
